a52c3784e2bfee62f23c9e8bdb8e700d426d9689
Charalampos Fanoulis <yo@fanoulis.dev>
2022-11-20 21:53:24 +0200

    uhh stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head.js';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+	return (
+		<>
+			<Head>
+				<title>FediFollowSync</title>
+				<link rel="stylesheet" href="https://unpkg.com/blocks.css/dist/blocks.min.css" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
