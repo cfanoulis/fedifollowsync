dad79d3e6d3006775e4852da5b2d1cdf83b7f369
Charalampos Fanoulis <yo@fanoulis.dev>
2022-11-21 17:10:09 +0200

    more css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import '../styles/normalize.css';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
