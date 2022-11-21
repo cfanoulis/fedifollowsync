cabe86694e31381e3cb7a961c3506d5e292cb552
Charalampos Fanoulis <yo@fanoulis.dev>
2022-11-21 17:38:13 +0200

    use different twitter sdk

--- lib/manager/TwitterManager.ts
+++ lib/manager/TwitterManager.ts
@@ -1,31 +1,46 @@
-import { auth } from 'twitter-api-sdk';
-
-export default class TwitterManager {
-	private twitterAuth: auth.OAuth2User;
+import { TwitterApi, TwitterApiReadOnly } from 'twitter-api-v2';
+export class TwitterManager {
+	private twitter: TwitterApiReadOnly;
 
 	public constructor() {
-		this.twitterAuth = new auth.OAuth2User({
-			client_id: process.env.TWT_ID!,
-			client_secret: process.env.TWT_SECRET!,
-			callback: 'http://localhost:3000/api/auth/twitter/callback',
-			scopes: ['users.read', 'offline.access']
+		// this.twitter = new auth.OAuth2User({
+		// 	client_id: ,
+		// 	client_secret: ,
+		// 	callback: 'http://localhost:3000/api/auth/twitter/callback',
+		// 	scopes: ['users.read', 'offline.access']
+		// });
+
+		this.twitter = new TwitterApi({ clientId: process.env.TWT_ID!, clientSecret: process.env.TWT_SECRET! }).readOnly;
+	}
+
+	public async prepareUrlLogin() {
+		const { url, codeVerifier, state } = this.twitter.generateOAuth2AuthLink('http://localhost:3000/api/auth/twitter/callback', {
+			scope: TwitterManager.SCOPES
 		});
+
+		return { url, codeVerifier, state };
 	}
 
-	public prepareUrlLogin(state: string) {
-		return this.twitterAuth.generateAuthURL({
-			code_challenge_method: 's256',
-			state: Buffer.from(state).toString('base64url')
+	public async exchangeCode(code: string, codeVerifier: string) {
+		const { accessToken, refreshToken, expiresIn } = await this.twitter.loginWithOAuth2({
+			code,
+			codeVerifier,
+			redirectUri: 'http://localhost:3000/api/auth/twitter/callback'
 		});
+
+		return {
+			accessToken,
+			refreshToken,
+			expiresIn
+		};
 	}
 
-	public async exchangeCode(code: string) {
-		return this.twitterAuth.requestAccessToken(code);
-	}
-
-	public static SCOPES = ['offline.access', 'users.read'].join('%20');
+	public static SCOPES = ['offline.access', 'users.read'];
 	public static HEADERS = {
-		'User-Agent': 'FediFollowSync/v0 - ffs@fanoulis.dev',
-		Authentication: `Basic ${Buffer.from(`${process.env.TWT_ID}:${process.env.TWT_SECRET}`).toString('base64')}`
+		'User-Agent': 'FediFollowSync/v0 - ffs@fanoulis.dev'
+		// Authentication: `Basic ${Buffer.from(`${process.env.TWT_ID}:${process.env.TWT_SECRET}`).toString('base64')}`
 	};
 }
+
+const twt = new TwitterManager();
+export default twt;
--- pages/api/auth/twitter/init.ts
+++ pages/api/auth/twitter/init.ts
@@ -1,7 +1,13 @@
-import TwitterManager from 'lib/manager/TwitterManager';
+import twt from 'lib/manager/TwitterManager';
+import { withSessionRoute } from 'lib/session';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<{}>) {
-	const url = new TwitterManager().prepareUrlLogin('hahaveryfunny');
+export default withSessionRoute(async function handler(req: NextApiRequest, res: NextApiResponse<{}>) {
+	const { url, state, codeVerifier } = await twt.prepareUrlLogin();
+
+	req.session.state = state;
+	req.session.code_challenge = codeVerifier;
+	await req.session.save();
+
 	res.redirect(307, url);
-}
+});
